e292390ec39c409c59449fd591c3e03296d0b963
Varkaria <61514399+HakuraSukinoe@users.noreply.github.com>
2020-08-23 15:57:27 +0700

    oh i forgot this ha lol

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,7 @@ const Nav = ()=>{
     return(
         <nav className="nav" id="navbar">
             <div className="nav-wrapper">
-            <p className="nav-right">ThisTine</p>
+            <p className="nav-right">ความคิดเห็นเพื่อโรงเรียนของเรา</p>
             <a className="btn nav-btn nav-left" href="https://forms.gle/AawBpacvWVf2woo36">กรอกแบบสอบถาม</a>
             </div>
         </nav>
